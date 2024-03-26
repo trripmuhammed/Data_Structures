e092631a7f57cfe932f3cadc456e5dcb57076daa
e y <muhammed53m@gmail.com>
2024-03-26 13:50:18 +0300

    v1

---
+++ main/day02/C01.py
@@ -0,0 +1,34 @@
+from array import *
+dizi=array("i",[21,16,56,82,34])
+print(dizi)
+
+array('i',[21,16,56,72,34])
+
+tuple1=()
+print(tuple1)
+tuple1=(10,"Hasan",7.9)
+print(tuple1)
+
+demet=()
+print(demet)
+demet = ("elma",[12,45,52],(14,21,34))
+print(demet)
+
+demet2=('p','y','t','h','o','n')
+print(demet2[1])
+print(demet2[-1])
+
+print(demet[0][2])
+print(demet[1][2])
+
+print(demet2[1:4])
+print(demet2[:])
+
+print('t' in demet2) #demetin icinde olup olmadigini kontrol eder bool olarak dondurur
+print(21 in demet[2])
+print([12,45,52] in demet)
+
+dizi=[8,10,12]
+yeni = tuple(dizi)
+print(type(yeni))
+
---
+++ main/day02/C02.py
@@ -0,0 +1,15 @@
+S1={1:"bir",2:"iki",3:"uc",4:"dort",5:"bes"}
+S2={"beyaz":"whilte","siyah":"black","pembe":"pink","yesil":"green","sari":"yellow"}
+print(S1)
+print(S2)
+
+S3={"ad":"mustafa","soyad":"kaya","yas":10}
+print(S3)
+
+renkler = S2.copy()
+print(renkler)
+
+dersler=("a","b","c","d")
+notlar=50
+sozluk=dict.fromkeys(dersler,notlar)
+print(sozluk)
